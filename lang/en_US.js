1b215d9a587404cc184c71139152f0a85963b4ed
Mason Dye <108563824+MasonDye@users.noreply.github.com>
2023-02-18 14:39:01 +0800

    V1

---
+++ lang/en_US.js
@@ -0,0 +1,20 @@
+/*
+* [CC Attack ++ Language en_US (English)] Power by Mason
+* Version : V1.5.1 (BUILD_2023_2_17_21:10)
+* Email : admin@almondnet.cn
+* Github : https://github.com/MasonDye/CCAttack++
+*/
+
+export var en_US = {
+    m_URL : "URL",
+    m_postSpeed : "POST request speed",
+    m_postContent : "POST request content",
+    m_postHeader : "POST request header application/x-www-form-urlencoded",
+    m_start : "Start",
+    m_stop : "Stop",
+    m_notURL : "Please enter the URL that needs to be CC",
+    m_notHttp : "Please enter a URL starting with http or https",
+    m_powerBy : "Power by",
+    m_version : "Version",
+    m_attackNumber : "Number of attacks",
+};
---
+++ lang/zh_CN.js
@@ -0,0 +1,20 @@
+/*
+* [CC Attack ++ Language zh_CN (Chinese)] Power by Mason
+* Version : V1.5.1 (BUILD_2023_2_17_21:10)
+* Email : admin@almondnet.cn
+* Github : https://github.com/MasonDye/CCAttack++
+*/
+
+export var zh_CN = {
+    m_URL : "网址",
+    m_postSpeed : "POST请求速度",
+    m_postContent : "POST请求内容",
+    m_postHeader : "POST请求头 application/x-www-form-urlencoded",
+    m_start : "开始",
+    m_stop : "停止",
+    m_notURL : "请输入需要被CC的网址",
+    m_notHttp : "请输入http或https开头的网址",
+    m_powerBy : "作者",
+    m_version : "版本",
+    m_attackNumber : "攻击次数",
+};
